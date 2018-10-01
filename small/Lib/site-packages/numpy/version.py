1194a8048cc99f4c3c142ab8c64c90776ee43890
AmyLi98 <1758870781@qq.com>
2018-10-01 20:35:46 +0800

    modify gitignore

---
+++ small/Lib/site-packages/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.15.1'
+version = '1.15.1'
+full_version = '1.15.1'
+git_revision = 'd24557104def1dec25d098c709c9a31e67c6d961'
+release = True
+
+if not release:
+    version = full_version
